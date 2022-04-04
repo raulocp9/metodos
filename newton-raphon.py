9f933e5d1e0786aed587cb76b11f7cb63ba70100
Raul <rocaluar9797@gmail.com>
2022-04-03 23:35:15 -0500

    programa funcionando :)

--- newton-raphon.py
+++ newton-raphon.py
@@ -2,39 +2,44 @@ import numpy as np
 import math 
 
 def func1(x):
-    f1 = x[0]**2 + x[0]*x[1] -10
-    f2 = x[1] + 3*x[0]*x[1]**2 -50
+    f1 = x[0][0]**2 + x[0][0]*x[1][0] -10
+    f2 = x[1][0] + 3*x[0][0]*x[1][0]**2 -50
     return np.array([[f1],[f2]])
 
 def J_func1(x):
-    return np.array([[2*x[0] + 1, x[0]],
-                    [3*x[1]**2, 6*x[0]*x[1] + 1]])
+    return np.array([[2*x[0][0] + x[1][0], x[0][0]],
+                    [3*x[1][0]**2, 6*x[0][0]*x[1][0] + 1]])
 
 def func2(X):
-    f1 = x[0]**2 + x[1]**2 -9
-    f2 = -np.exp(1)**2
-    return np.matrix([[f1],[f2]])
+    f1 = x[0][0]**2 + x[1][0]**2 -9
+    f2 = -np.exp(x[0][0])-2*x[1][0]-3
+    return np.array([[f1],[f2]])
 
 def J_func2(x):
-    return np.matrix([[2*x[0], 2*x[1]],[-np.exp(1)^x[0], -2]])
+    return np.array([[2*x[0][0], 2*x[1][0]],
+                    [-np.exp(x[0][0]), -2]])
 
 def func3(x):
-    f1 = 2*x[0]**2 - 4*x[0] + x[1]**2
-    f2 = x[0]**2 + x[1]**2 -2*x[1] +2*x[2]**2 -5
-    f3 = 2*x[0]**2 -12*x[0] + x[1]**2 -3*x[2] +8
-    return np.matrix([[f1],[f2],[f3]])
+    f1 = 2*x[0][0]**2 - 4*x[0][0] + x[1][0]**2 + 3*x[2][0]**2 + 6*x[2][0] + 2
+    f2 = x[0][0]**2 + x[1][0]**2 - 2*x[1][0] + 2*x[2][0]**2 -5
+    f3 = 3*x[0][0]**2 - 12*x[0][0] + x[1][0]**2 - 3*x[2][0]**2 + 8
+    return np.array([[f1],[f2],[f3]])
 
 def J_func3(x):
-    return np.matrix([[4*x[0] -4, 2*x[1], 6*x[2] + 6],[2*x[0], 2*x[1] -2, 4*x[2]],[6*x[0] -12, 2*x[1], -6*x[2]]])
+    return np.array([[4*x[0][0] -4, 2*x[1][0], 6*x[2][0] + 6],
+                    [2*x[0][0], 2*x[1][0] -2, 4*x[2][0]],
+                    [6*x[0][0] -12, 2*x[1][0], -6*x[2][0]]])
 
 def func4(x):
-    f1 = x[0]**2 -4*x[0] + x[1]**2
-    f2 = x[0]**2 -x[0] -12*x[1] +1
-    f3 = 3*x[0]**2 -12*x[0] + x[1]**2 -3*x[2]**2 + 8
-    return np.matrix([[f1],[f2],[f3]])
+    f1 = x[0][0]**2 -4*x[0][0] + x[1][0]**2
+    f2 = x[0][0]**2 -x[0][0] -12*x[1][0] +1
+    f3 = 3*x[0][0]**2 -12*x[0][0] + x[1][0]**2 -3*x[2][0]**2 + 8
+    return np.array([[f1],[f2],[f3]])
 
 def J_func4(x):
-    return np.matrix([[2*x[0] -4, 2*x[1], 0],[2*x[0] -1, -12, 0],[6*x[0] -12, 2*x[1], -6*x[2]]])
+    return np.array([[2*x[0][0] -4, 2*x[1][0], 0],
+                    [2*x[0][0] -1, -12, 0],
+                    [6*x[0][0] -12, 2*x[1][0], -6*x[2][0]]])
 
 
 def menu():
@@ -69,32 +74,31 @@ def menu():
                 print("Out of range. Try again\n")
     return opc
 
-
 def menu2():
     print("Enter the maximum number of iterations")
     iteration = int(input())
-    print("Enter the maximum Et(True Error)")
+    print("Enter the maximum Ea(Absolute Error)")
     error = float(input())
     return iteration, error
 
 def values1():
-    x = np.array([1,1])
+
     print("Enter the initial value of x")
-    x[0] = float(input())
+    x = float(input())
     print("Enter the initial value of y")
-    x[1] = float(input())
+    y = float(input())
     print("\n")
-    return np.array(x)
+    return np.array([[x],[y]])
 
 def values2():
     print("Enter the initial value of x")
-    x[0] = int(input())
+    a  = float(input())
     print("Enter the initial value of y")
-    x[1] = int(input())
+    b = float(input())
     print("Enter the initial values of z")
-    x[2] = int(input())
+    c = float(input())
     print("\n")
-    return x[0], x[1], x[2]
+    return np.array([[a],[b],[c]])
 
 
 if __name__=="__main__":
@@ -102,33 +106,88 @@ if __name__=="__main__":
     if opc == 1:#newton-raphon method
         iteration, error = menu2() # max number of iterations and max error given by user
         x = values1()# initial point given by user
-
+ 
         for i in range(iteration):
             xold = x
             J_inv = np.linalg.inv(J_func1(x))
             product = np.dot(J_inv, func1(x))
-            print(x)
-            x = x- product# this line is the problem
-            print(x)
-            print(''' Iteration,\t product\t x
-{}        {}     {}
+            x = np.subtract(x,product)
+            err = np.linalg.norm(x -xold)
+            print(
+''' Iteration,\t x\t error
+    {}        {}       {:.6f}
             
             
-             '''.format(i, product, x) )
-            #err = np.linalg.norm(x -xold)
-            #if err.all() < error:
-             #   break
+             '''.format(i, x, err) )
+
+            if err < error:
+                break
+        else:
+            print("max error could not be reached")
 
     elif opc == 2:
         iteration, error = menu2()
+        x = values1()
+
+        for i in range(iteration):
+            xold = x
+            J_inv = np.linalg.inv(J_func2(x))
+            product = np.dot(J_inv, func2(x))
+            x = np.subtract(x,product)
+            err = np.linalg.norm(x -xold)
+            print(
+''' Iteration,\t x\t error
+    {}        {}       {:.6f}
+            
+            
+             '''.format(i, x, err) )
+
+            if err < error:
+                break
+        else:
+            print("max error could not be reached")
         
     elif opc == 3:
         iteration, error = menu2()
-        
+        x = values2()# initial point given by user
+ 
+        for i in range(iteration):
+            xold = x
+            J_inv = np.linalg.inv(J_func3(x))
+            product = np.dot(J_inv, func3(x))
+            x = np.subtract(x,product)
+            err = np.linalg.norm(x -xold)
+            print(
+''' Iteration,\t x\t error
+    {}        {}       {:.6f}
+            
+            
+             '''.format(i, x, err) )
+            if err < error:
+                break
+        else:
+            print("max error could not be reached")        
     elif opc ==4:
         iteration, error = menu2()
-        
+        x = values2()# initial point given by user
+ 
+        for i in range(iteration):
+            xold = x
+            J_inv = np.linalg.inv(J_func4(x))
+            product = np.dot(J_inv, func4(x))
+            x = np.subtract(x,product)
+            err = np.linalg.norm(x -xold)
+            print(
+''' Iteration,\t x\t error
+    {}        {}       {:.6f}
+            
+            
+             '''.format(i, x, err) )
+            if err < error:
+                break        
+        else:
+            print("max error could not be reached")
+
     elif opc ==5:
         quit()
-
 
