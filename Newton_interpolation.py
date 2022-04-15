380fed3989e56d51d3f76d93b63901d0cde1ca6c
Raul <rocaluar9797@gmail.com>
2022-04-15 02:07:12 -0500

    working

--- Newton_interpolation.py
+++ Newton_interpolation.py
@@ -52,8 +52,11 @@ def menu():
                     line = int(input())
                 except ValueError:
                     print("Not a number. Try again")
-                else: 
-                    break
+                else:
+                    if(line > n or line <= 0 ):
+                        print("Not a valid line. Try again")
+                    else: 
+                        break
             line = line -1
             while True:
                 print('Enter the new x value of the point {}'.format(line+1))
@@ -136,8 +139,8 @@ def select_backward_forward(poit, x):
             continue
         else:
             break
- 
-    forward_degree = (len(x)-1)-start
+
+    forward_degree = len(x)-start-1
     backward_degree = start+1
     print("\n\tEnter an option:")
     print("\n1. Newton forward, max degree of polynomial {}".format(forward_degree))
@@ -147,10 +150,11 @@ def select_backward_forward(poit, x):
             opc = int(input())
         except ValueError:
             print("Not a number. Try again")
-        if(opc < 1 or opc > 2):
-            print('Out of range. Try again')
         else:
-            break
+            if(opc < 1 or opc > 2):
+                print('Out of range. Try again')
+            else:
+                break
     return opc, start, forward_degree, backward_degree
     
 def backward_forward(start, opc, x, point, forward_degree, backward_degree):
@@ -181,7 +185,23 @@ def forward(start, x, point, forward_degree):
         matrix[i][0] = x[j][0]
         matrix[i][1] = x[j][1]
         i+=1
-    #print(matrix) 
+    print(matrix)
+    for i in range(2 ,len(matrix)+1):
+        for j in range(len(matrix)-i+1):
+            matrix[j][i] = matrix[j+1][i-1]-matrix[j][i-1]
+    print(matrix) 
+    h = np.absolute(matrix[0][0]-matrix[1][0])
+    s = (point-matrix[0][0])/h
+    y = matrix[0][1]
+    s1 = s
+    fact = 1
+    for k in range(2,len(matrix[1])):
+        yold = y
+        y = y+(s1*matrix[0][k]/fact)
+        fact = fact*k
+        s1 = s1*(s-(k-1))
+    print("Value of the interpolation {:.8f}\t {:.8f}".format(y, y-yold))     
+     
 
 def backward(x, point, backward_degree):
     if(backward_degree == 0):
@@ -194,18 +214,72 @@ def backward(x, point, backward_degree):
         except ValueError:
             print("Not a number. Try again")
         else:
-            if(degree > forward_degree or degree <= 0):
+            if(degree > backward_degree or degree <= 0):
                 print("Out of range")
             else:
                 break
 
-if __name__=="__main__":
-    x, y = menu()
-    matrix = sort(x, y)
-    check_equidistant(matrix)
-    point = menu2(matrix)
-    opc, start, forward_degree, backward_degree= select_backward_forward(point, matrix)
-    backward_forward(start, opc, matrix, point, forward_degree, backward_degree)
+    matrix = np.full((degree+1,degree+2),0)
 
-    
+    for j in range(backward_degree+1):
+        matrix[j][0] = x[j][0]
+        matrix[j][1] = x[j][1]
+    print(matrix)
+    for i in range(2 ,len(matrix)+1):
+        for j in range(len(matrix)-i+1):
+            matrix[j][i] = matrix[j+1][i-1]-matrix[j][i-1]
+    print(matrix) 
+    print(degree)
+    print(len(matrix))
+    print(len(matrix[1]))
+    print(matrix.shape)
+    h = np.absolute(matrix[0][0]-matrix[1][0])
+    s = (point-matrix[len(matrix)-1][0])/h
+    y = matrix[len(matrix)-1][1]
+    s1 = s
+    fact = 1
+    for k in range(2,len(matrix[0])): 
+        yold = y
+        y = y+(s1*matrix[len(matrix)-k][k]/fact)
+        fact = fact*k
+        s1 = s1*(s-(k-1))
+    print("Value of the interpolation {:.8f}\t {:.8f}".format(y, y-yold))     
+
+if __name__=="__main__":
+    while True:
+        x, y = menu()
+        while True:
+            matrix = sort(x, y)
+            check_equidistant(matrix)
+            point = menu2(matrix)
+            opc, start, forward_degree, backward_degree= select_backward_forward(point, matrix)
+            backward_forward(start, opc, matrix, point, forward_degree, backward_degree)
+            print("Do you want to intepolate another point with the same table?(Y/n)")
+            try:
+                option = input()
+            except ValueError:
+                print("Not an option. Try again")
+            else:
+                if(option == 'n' or option =='N'):
+                    break
+                else:
+                    continue
+        print('\n\tEnter an option')
+        print('1. Go back to main menu')
+        print('2. Exit')
+        while True:
+            try:
+                option2 = int(input())
+            except ValueError:
+                print('Not a number. Try again')
+            else:
+                if(option2<1 or option2 > 2):
+                    print('Out of range. Try again')
+                else: 
+                    break
+        if(option == 2):
+            exit()
+        else: 
+            continue    
+        
     
